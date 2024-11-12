b18505185df6c71ed56c365dbacfa02555e898c5
abhinavsrivas7 <130465627+abhinavsrivas@users.noreply.github.com>
2024-11-12 23:39:28 +0530

    added microphone icon

--- components/ChamatoSearchBar/ChamatorSearchBar.tsx
+++ components/ChamatoSearchBar/ChamatorSearchBar.tsx
@@ -1,12 +1,19 @@
 import { useState } from 'react';
 import { ChamatoSearchBarProps } from './ChamatoSearchBarTypes';
-import { Text } from 'react-native';
+import { SearchBar } from '@rneui/themed';
+import Entypo from '@expo/vector-icons/Entypo';
+import { IconNode } from '@rneui/base';
 
 export default function ChamatoSearchBar({placeholder}: ChamatoSearchBarProps) {
     const [isListening, setIsListening] = useState<boolean>(false);
     const [searchText, setSearchText] = useState<string>(placeholder ?? '');
-
+    const t: IconNode = <Entypo name="mic" size={24} color="gray" />
     return (
-        <Text />
+        <SearchBar
+            placeholder="Type Here..."
+            onChangeText={text => setSearchText(text)}
+            value={searchText}
+            clearIcon={t}
+        />
     );
 };
